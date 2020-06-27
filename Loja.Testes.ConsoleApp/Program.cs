da0e3948b5624b3b087a3c5463d200b14576034f
Everton Mendonça <evertonavilamendonca@gmail.com>
2020-06-26 21:58:18 -0300

    Adicionando o Entity no projeto e criando um exemplo de adição de dados no banco

--- Loja.Testes.ConsoleApp/Program.cs
+++ Loja.Testes.ConsoleApp/Program.cs
@@ -10,7 +10,33 @@ namespace Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            //GravarUsandoAdoNet();
+            GravarUsandoEntity();
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            Produto p2 = new Produto();
+            p2.Nome = "Senhor dos Anéis 1";
+            p2.Categoria = "Livros";
+            p2.Preco = 19.89;
+
+            Produto p3 = new Produto();
+            p3.Nome = "O Monge e o Executivo";
+            p3.Categoria = "Livros";
+            p3.Preco = 19.89;
+
+            using (var context = new LojaContext())
+            {
+                context.Produtos.AddRange(p, p2, p3);
+                context.SaveChanges();
+                
+            }
         }
 
         private static void GravarUsandoAdoNet()
